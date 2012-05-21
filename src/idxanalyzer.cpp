b47dbada48b4804a2de5d66d5c58d3044fd5e653
Jun He <junnhe@gmail.com>
2012-05-21 07:18:53 -0700

    added index analyzer files

---
+++ src/idxanalyzer.cpp
@@ -0,0 +1,496 @@
+#include "idxanalyzer.h"
+
+#include <algorithm>
+
+//for debugging
+void printIdxEntries( vector<IdxSigEntry> &idx_entry_list )
+{
+    vector<IdxSigEntry>::const_iterator iter;
+
+    //cout << "this is printIdxEntries" << endl;
+
+    for ( iter = idx_entry_list.begin();
+            iter != idx_entry_list.end();
+            iter++ )
+    {
+        cout << "[" << iter->proc << "]" << endl;
+        cout << "----Logical Offset----" << endl;
+        iter->logical_offset.show();
+        
+        vector<IdxSigUnit>::const_iterator iter2;
+
+        cout << "----Length----" << endl;
+        for (iter2 = iter->length.begin();
+                iter2 != iter->length.end();
+                iter2++ )
+        {
+            iter2->show(); 
+        }
+
+        cout << "----Physical Offset----" << endl;
+        for (iter2 = iter->physical_offset.begin();
+                iter2 != iter->physical_offset.end();
+                iter2++ )
+        {
+            iter2->show(); 
+        }
+        cout << "-------------------------------------" << endl;
+    }
+}
+
+vector<off_t> buildDeltas( vector<off_t> seq ) 
+{
+    vector<off_t>::iterator it;
+    vector<off_t> deltas;
+    for ( it = seq.begin() ; it != seq.end() ; it++ )
+    {
+        if ( it > seq.begin() ) {
+            deltas.push_back( *it - *(it-1) );
+        }
+    }
+    //cout << "in builddeltas: " << seq.size() << " " << deltas.size() << endl; 
+    return deltas;
+}
+
+//IdxEntry is designed to mimic the HostEntry in PLFS,
+//so later I can easily copy and paste these code to PLFS
+//and make it work.
+//
+//It gets signatures for a proc. 
+//TODO:
+//But do we really need to separate entries by proc at first?
+//Also, have to handle the case that there is only one entry
+IdxSigEntryList IdxSignature::generateIdxSignature(vector<IdxEntry> &entry_buf, 
+                                        int proc) 
+{
+    vector<off_t> logical_offset, length, physical_offset; 
+    vector<off_t> logical_offset_delta, 
+                    length_delta, 
+                    physical_offset_delta;
+    IdxSigEntryList entrylist;
+    static int totalsize = 0;
+    vector<IdxEntry>::const_iterator iter;
+    //cout<< "i am in generateIdxSignature" << endl << "entry_buf.size()=" << entry_buf.size() << endl;
+    for ( iter = entry_buf.begin() ; 
+            iter != entry_buf.end() ;
+            iter++ )
+    {
+        if ( iter->Proc != proc ) {
+            continue;
+        }
+
+        logical_offset.push_back(iter->Logical_offset);
+        length.push_back(iter->Length);
+        physical_offset.push_back(iter->Physical_offset);
+    }
+    
+    logical_offset_delta = buildDeltas(logical_offset);
+    length_delta = buildDeltas(length);
+    physical_offset_delta = buildDeltas(physical_offset);
+
+    SigStack<IdxSigUnit> offset_sig = 
+        discoverSigPattern(logical_offset_delta, logical_offset);
+    //offset_sig.show();
+    //cout << "Just showed offset_sig" << endl;
+    //Now, go through offset_sig one by one and build the IdxSigEntry s
+    vector<IdxSigEntry>idx_entry_list;
+    vector<IdxSigUnit>::const_iterator stack_iter;
+
+    int range_start = 0, range_end; //the range currently processing
+    for (stack_iter = offset_sig.begin();
+            stack_iter != offset_sig.end();
+            stack_iter++ )
+    {
+        //cout << stack_iter->init << " " ;
+        IdxSigEntry idx_entry;
+        range_end = range_start + stack_iter->size();
+
+        vector<off_t>::iterator lstart, lend;
+        lstart = length_delta.begin() + range_start;
+        if ( length_delta.end() - (length_delta.begin() + range_end) > 0 ) {
+            lend = length_delta.begin() + range_end;
+        } else {
+            lend = length_delta.end();
+        }
+        SigStack<IdxSigUnit> length_stack = 
+            discoverSigPattern( 
+                    vector<off_t> (lstart, lend),
+                    vector<off_t> (length.begin()+range_start,
+                        length.begin()+range_end) ); //this one pointed by length.begin()+range_end 
+                                                     //won't be paseed in. The total size passed is
+                                                     //stack_iter.size();
+        //cout << "************End length" << endl;
+
+        SigStack<IdxSigUnit> physical_offset_stack = 
+            discoverSigPattern( 
+                    vector<off_t>(lstart, lend),
+                    vector<off_t> (physical_offset.begin()+range_start,
+                        physical_offset.begin()+range_end) );
+
+        idx_entry.proc = proc;
+        idx_entry.logical_offset = *stack_iter;
+        idx_entry.length = length_stack;
+        idx_entry.physical_offset = physical_offset_stack;
+        
+        idx_entry_list.push_back( idx_entry);
+        totalsize += idx_entry.memsize();
+
+        range_start = range_end;
+    }
+    entrylist.append(idx_entry_list);
+    //printIdxEntries(idx_entry_list);
+    fprintf(stderr, "so far(proc:%d), total size is: %d Bytes (%d KB).\n", 
+            proc, totalsize, totalsize/1024);
+    return entrylist;
+}
+
+
+
+//find out pattern of a number sequence 
+void IdxSignature::discoverPattern(  vector<off_t> const &seq )
+{
+    vector<off_t>::const_iterator p_lookahead_win; // pointer(iterator) to the lookahead window
+    PatternStack<PatternUnit> pattern_stack;
+
+    p_lookahead_win = seq.begin();
+    pattern_stack.clear();
+
+    //cout << endl << "this is discoverPattern() :)" << endl;
+
+    while ( p_lookahead_win != seq.end() ) {
+        //lookahead window is not empty
+        Tuple cur_tuple = searchNeighbor( seq, p_lookahead_win );
+        //cur_tuple.show();
+        if ( cur_tuple.isRepeatingNeighbor() ) {
+            if ( pattern_stack.isPopSafe( cur_tuple.length ) ) {
+                //safe
+                pattern_stack.popElem( cur_tuple.length );
+
+                vector<off_t>::const_iterator first, last;
+                first = p_lookahead_win;
+                last = p_lookahead_win + cur_tuple.length;
+
+                PatternUnit pu;
+                pu.seq.assign(first, last);
+                pu.cnt = 2;
+
+                pattern_stack.push( pu );
+                p_lookahead_win += cur_tuple.length;
+            } else {
+                //unsafe
+                PatternUnit pu = pattern_stack.top();
+
+                if ( pu.seq.size() == cur_tuple.length ) {
+                    //the subseq in lookahead window repeats
+                    //the top pattern in stack
+                    pu.cnt++;
+                    pattern_stack.popPattern();
+                    pattern_stack.push(pu);
+
+                    p_lookahead_win += cur_tuple.length;
+                } else {
+                    //cannot pop out cur_tuple.length elems without
+                    //totally breaking any pattern.
+                    //So we simply add one elem to the stack
+                    PatternUnit pu2;
+                    pu2.seq.push_back( *p_lookahead_win );
+                    pu2.cnt = 1;
+                    pattern_stack.push(pu2);
+                    p_lookahead_win++;
+                }
+            }
+
+
+        } else {
+            //(0,0,x)
+            PatternUnit pu;
+            pu.seq.push_back(cur_tuple.next_symbol);
+            pu.cnt = 1;
+
+            pattern_stack.push(pu); 
+            p_lookahead_win++;
+        }
+        //pattern_stack.show();
+    }
+
+}
+
+//find out pattern of a number sequence(deltas) with its
+//original sequence
+//if seq and orig have the same sizes.
+//  the function returns pattern representing all orig numbers.
+//else if orig has one more than seq (including seq.size()==0)
+//  the function returns pattern representing all orig numbers, 
+//  with the last orig num with seq.size()==0
+//else
+//  error
+SigStack<IdxSigUnit> IdxSignature::discoverSigPattern( vector<off_t> const &seq,
+        vector<off_t> const &orig )
+{
+    // pointer(iterator) to the lookahead window, bot should move together
+    vector<off_t>::const_iterator p_lookahead_win, 
+        p_lookahead_win_orig; 
+    SigStack<IdxSigUnit> pattern_stack;
+
+    p_lookahead_win = seq.begin();
+    p_lookahead_win_orig = orig.begin();
+    pattern_stack.clear();
+
+    if (! (seq.size() == orig.size()
+            || seq.size() + 1 == orig.size() ) )
+    {
+        fprintf(stderr, "discoverSigPattern() needs to be used with "
+                "seq.size==orig.size or seq.size+1==orig.size");
+        exit(-1);
+    }
+
+    //cout << endl << "this is discoverPattern() :)" << endl;
+   
+    //TODO:
+    //There's bug in handling the case of only one entry.
+    //And whether 1,(3,4)^2 represnets five or four numbers.
+    //Go back to handle this when protoc buffer is integrated.
+    /*
+    cout << "seq.size(): " << seq.size() << "orig.size():" << orig.size() << endl;
+    assert(seq.size() == (orig.size()-1));
+
+    //for the case there is only one entry
+    if ( seq.size() == 0 ) {
+        IdxSigUnit pu;
+        pu.init = *p_lookahead_win_orig;
+        pu.cnt = 0;
+    }
+    */    
+
+
+
+    //TODO: WHY p_lookahead_win != seq.end() is a dead loop????
+    while ( p_lookahead_win < seq.end() ) {
+        //lookahead window is not empty
+        Tuple cur_tuple = searchNeighbor( seq, p_lookahead_win );
+        //cur_tuple.show();
+        if ( cur_tuple.isRepeatingNeighbor() ) {
+            if ( pattern_stack.isPopSafe( cur_tuple.length ) ) {
+                //safe
+                pattern_stack.popElem( cur_tuple.length );
+
+                vector<off_t>::const_iterator first, last;
+                first = p_lookahead_win;
+                last = p_lookahead_win + cur_tuple.length;
+
+                IdxSigUnit pu;
+                pu.seq.assign(first, last);
+                pu.cnt = 2;
+                pu.init = *(p_lookahead_win_orig - cur_tuple.length);
+
+                pattern_stack.push( pu );
+                p_lookahead_win += cur_tuple.length;
+                p_lookahead_win_orig += cur_tuple.length;
+            } else {
+                //unsafe
+                IdxSigUnit pu = pattern_stack.top();
+
+                if ( pu.seq.size() == cur_tuple.length ) {
+                    //the subseq in lookahead window repeats
+                    //the top pattern in stack.
+                    //initial remains the same.
+                    pu.cnt++;
+                    pattern_stack.popPattern();
+                    pattern_stack.push(pu);
+                    pu.init = *p_lookahead_win_orig; //should delete this. keep if only for 
+                    //tmp debug.
+
+                    p_lookahead_win += cur_tuple.length;
+                    p_lookahead_win_orig += cur_tuple.length;
+                } else {
+                    //cannot pop out cur_tuple.length elems without
+                    //totally breaking any pattern.
+                    //So we simply add one elem to the stack
+                    IdxSigUnit pu;
+                    pu.seq.push_back( *p_lookahead_win );
+                    pu.init = *p_lookahead_win_orig;
+                    pu.cnt = 1;
+                    pattern_stack.push(pu);
+                    p_lookahead_win++;
+                    p_lookahead_win_orig++;
+                }
+            }
+        } else {
+            //(0,0,x)
+            IdxSigUnit pu;
+            pu.seq.push_back(cur_tuple.next_symbol);
+            pu.init = *p_lookahead_win_orig;
+            pu.cnt = 1;
+
+            pattern_stack.push(pu); 
+            p_lookahead_win++;
+            p_lookahead_win_orig++;
+        }
+    }
+   
+    if ( p_lookahead_win_orig < orig.end() ) {
+        assert(p_lookahead_win_orig + 1 == orig.end());
+        IdxSigUnit pu;
+        pu.init = *p_lookahead_win_orig;
+        pu.cnt = 0;
+
+        pattern_stack.push(pu); 
+    }
+
+    return pattern_stack;
+}
+
+Tuple IdxSignature::searchNeighbor( vector<off_t> const &seq,
+        vector<off_t>::const_iterator p_lookahead_win ) 
+{
+    vector<off_t>::const_iterator i;     
+    int j;
+    //cout << "------------------- I am in searchNeighbor() " << endl;
+
+    //i goes left util the begin or reaching window size
+    int distance = 0;
+    i = p_lookahead_win;
+    while ( i != seq.begin() && distance < win_size ) {
+        i--;
+        distance++;
+    }
+    //termination: i == seq.begin() or distance == win_size
+
+    /*
+    //print out search buffer and lookahead buffer
+    //cout << "search buf: " ;
+    vector<off_t>::const_iterator k;
+    for ( k = i ; k != p_lookahead_win ; k++ ) {
+    cout << *k << " ";
+    }
+    cout << endl;
+
+    cout << "lookahead buf: " ;
+    vector<off_t>::const_iterator p;
+    p = p_lookahead_win;
+    for ( p = p_lookahead_win ; 
+    p != seq.end() && p - p_lookahead_win < win_size ; 
+    p++ ) {
+    cout << *p << " ";
+    }
+    cout << endl;
+    */
+
+    //i points to a element in search buffer where matching starts
+    //j is the iterator from the start to the end of search buffer to compare
+    for ( ; i != p_lookahead_win ; i++ ) {
+        int search_length = p_lookahead_win - i;
+        for ( j = 0 ; j < search_length ; j++ ) {
+            if ( *(i+j) != *(p_lookahead_win + j) ) {
+                break;
+            }
+        }
+        if ( j == search_length ) {
+            //found a repeating neighbor
+            return Tuple(search_length, search_length, 
+                    *(p_lookahead_win + search_length));
+        }
+    }
+
+    //Cannot find a repeating neighbor
+    return Tuple(0, 0, *(p_lookahead_win));
+}
+
+void IdxSigEntryList::append( vector<IdxSigEntry> &other ) 
+{
+    vector<IdxSigEntry>::iterator iter;
+    for (iter = other.begin();
+            iter != other.end();
+            iter++ )
+    {
+        list.push_back(*iter);
+    }
+
+}
+
+void IdxSigEntryList::append( IdxSigEntryList other ) 
+{
+    append(other.list);
+}
+
+void IdxSigEntryList::show()
+{
+    printIdxEntries(list);
+}
+
+
+
+void idxSigUnit2PBSigUnit( const IdxSigUnit &iunit, idxfile::SigUnit *pbunit )
+{
+    pbunit->set_init(iunit.init);
+    vector<off_t>::const_iterator iter;
+    for ( iter = iunit.seq.begin();
+            iter != iunit.seq.end();
+            iter++ )
+    {
+        pbunit->add_deltas(*iter);
+    }
+    pbunit->set_cnt(iunit.cnt);
+}
+
+void IdxSigEntryList::siglistToPblist(vector<IdxSigEntry> &slist,
+        idxfile::EntryList &pblist)
+{
+    //read out every entry in slist and put it to pblist
+    vector<IdxSigEntry>::iterator iter;
+
+    for ( iter = slist.begin();
+            iter != slist.end();
+            iter++)
+    {
+        idxfile::Entry *fentry = pblist.add_entry();
+        fentry->set_proc( (*iter).proc );
+        idxfile::SigUnit *su = fentry->mutable_logical_offset();
+        idxSigUnit2PBSigUnit( (*iter).logical_offset, su );
+
+        //length
+        vector<IdxSigUnit>::const_iterator iter2;
+        for ( iter2 = (*iter).length.begin();
+                iter2 != (*iter).length.end();
+                iter2++ )
+        {
+            idxfile::SigUnit *su = fentry->add_length();
+            idxSigUnit2PBSigUnit( (*iter2), su);
+        }
+
+        //physical offset
+        for ( iter2 = (*iter).length.begin();
+                iter2 != (*iter).length.end();
+                iter2++ )
+        {
+            idxfile::SigUnit *su = fentry->add_physical_offset();
+            idxSigUnit2PBSigUnit( (*iter2), su);
+        }
+    }
+}
+
+void IdxSigEntryList::saveToFile(char *filename)
+{
+    siglistToPblist(list, pb_list);
+    fstream output(filename, ios::out | ios::trunc | ios::binary);
+    if ( !pb_list.SerializeToOstream(&output) ) {
+        cerr<<"failed to write to myfile."<<endl;
+    } else {
+        cout<<"Write to myfile: OK"<<endl;
+    }
+    output.close();
+}
+
+void IdxSigEntryList::readFromFile(char *filename)
+{
+    fstream input(filename, ios::in | ios::binary);
+    if ( !input ) {
+        cerr << "can not open my file.\n";
+    } else if ( !pb_list.ParseFromIstream(&input) ) {
+        cerr << "failed to parse from myfile\n";
+    }
+    input.close();
+}
+
+
+
---
+++ src/idxanalyzer.h
@@ -0,0 +1,336 @@
+#ifndef __idxanalyzer_h__
+#define __idxanalyzer_h__
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "protobuf/index.pb.h"
+
+using namespace std;
+
+#define off_t long long int
+
+template <class T> class SigStack;
+template <class T> class PatternStack;
+class IdxSigEntryList;
+
+//used to describe a single pattern that found
+//This will be saved in the stack
+class PatternUnit {
+    public:
+        vector<off_t> seq;
+        int cnt; //count of repeatition
+        
+        PatternUnit() {}
+        PatternUnit( vector<off_t> sq, int ct )
+            :seq(sq),cnt(ct)
+        {}
+        //return number of elements in total
+        int size() const 
+        {
+            if ( cnt == 0 ) {
+                return 1; //not repetition
+            } else {
+                return seq.size()*cnt;
+            }
+        }
+        
+        int memsize() 
+        {
+            return sizeof(cnt) + sizeof(off_t)*seq.size();
+        }
+
+        virtual void show() const
+        {
+            vector<off_t>::const_iterator iter;
+            cout << "( " ;
+            for (iter = seq.begin();
+                    iter != seq.end();
+                    iter++ )
+            {
+                cout << *iter << " ";
+            }
+            cout << ") ^" << cnt << endl;
+        }
+};
+
+//This is used to describ a single repeating
+//pattern, but with starting value
+class IdxSigUnit: public PatternUnit {
+    public:
+        off_t init; // the initial value of 
+                    // logical offset, length, or physical offset
+        void show() const
+        {
+            cout << init << " ... ";
+            PatternUnit::show();
+        }
+
+        int memsize() {
+            return sizeof(init) + PatternUnit::memsize();
+        }
+};
+
+template <class T> // T can be PatternUnit or IdxSigUnit
+class PatternStack {
+    public:
+        PatternStack() {}
+        void push( T pu ) 
+        {
+            the_stack.push_back(pu);
+        }
+        
+        void clear() 
+        {
+            the_stack.clear();
+        }
+
+        //if popping out t elem breaks any patterns
+        bool isPopSafe( int t ) 
+        {
+            typename vector<T>::reverse_iterator rit;
+            
+            int total = 0;
+            rit = the_stack.rbegin();
+            while ( rit != the_stack.rend()
+                    && total < t )
+            {
+                total += rit->size();
+                rit++;
+            }
+            return total == t;
+        }
+
+        //return false if it is not safe
+        //t is number of elements, not pattern unit
+        bool popElem ( int t )
+        {
+            if ( !isPopSafe(t) ) {
+                return false;
+            }
+
+            int total = 0; // the number of elem already popped out
+            while ( !the_stack.empty() && total < t ) {
+                total += top().size();
+                the_stack.pop_back();
+            }
+            assert( total == t );
+
+            return true;
+        }
+
+        //pop out one pattern
+        void popPattern () 
+        {
+            the_stack.pop_back();
+        }
+        
+        //make sure the stack is not empty before using this
+        T top () 
+        {
+            assert( the_stack.size() > 0 );
+            return the_stack.back();
+        }
+
+        typename vector<T>::const_iterator
+            begin() const
+        {
+            return the_stack.begin();
+        }
+        
+        typename vector<T>::const_iterator
+            end() const
+        {
+            return the_stack.end();
+        }
+        
+        virtual void show()
+        {
+            typename vector<T>::const_iterator iter;
+            
+            for ( iter = the_stack.begin();
+                    iter != the_stack.end();
+                    iter++ )
+            {
+                iter->show();
+                /*
+                vector<off_t>::const_iterator off_iter;
+                for ( off_iter = (iter->seq).begin();
+                        off_iter != (iter->seq).end();
+                        off_iter++ )
+                {
+                    cout << *off_iter << ", ";
+                }
+                cout << "^" << iter->cnt << endl;
+                */
+            }
+        }
+    
+    protected:
+        vector<T> the_stack;
+};
+
+//I really should not call it a stack since I use
+//it in many ways...
+template <class T>
+class SigStack: public PatternStack <T> 
+{
+    public:
+        virtual void show()
+        {
+            typename vector<T>::const_iterator iter;
+
+            for ( iter = this->the_stack.begin();
+                    iter != this->the_stack.end();
+                    iter++ )
+            {
+                vector<off_t>::const_iterator off_iter;
+                cout << iter->init << "- " ;
+                for ( off_iter = (iter->seq).begin();
+                        off_iter != (iter->seq).end();
+                        off_iter++ )
+                {
+                    cout << *off_iter << ", ";
+                }
+                cout << "^" << iter->cnt << endl;
+            }
+        }
+
+        int memsize() 
+        {
+            typename vector<T>::const_iterator iter;
+            int totalsize = 0;
+            for ( iter = PatternStack<T>::the_stack.begin();
+                    iter != PatternStack<T>::the_stack.end();
+                    iter++)
+            {
+                totalsize += ((T)(*iter)).memsize();
+            }
+            return totalsize;
+        }
+
+};
+
+class Tuple {
+    public:
+        int offset; //note that this is not the 
+        // offset when accessing file. But
+        // the offset in LZ77 algorithm
+        int length; //concept in LZ77
+        off_t next_symbol;
+
+        Tuple() {}
+        Tuple(int o, int l, off_t n) {
+            offset = o;
+            length = l;
+            next_symbol = n;
+        }
+
+        void put(int o, int l, off_t n) {
+            offset = o;
+            length = l;
+            next_symbol = n;
+        }
+
+        bool operator== (const Tuple other) {
+            if (offset == other.offset 
+                    && length == other.length
+                    && next_symbol == other.next_symbol)
+            {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Tell if the repeating sequences are next to each other
+        bool isRepeatingNeighbor() {
+            return (offset == length && offset > 0);
+        }
+
+        void show() {
+            cout << "(" << offset 
+                << ", " << length
+                << ", " << next_symbol << ")" << endl;
+        }
+};
+
+
+class IdxEntry {
+    public:
+        int Proc;
+#define ID_WRITE 0
+#define ID_READ  1
+        int ID; //either ID_WRITE or ID_READ
+        off_t Logical_offset;
+        off_t Length;
+        double Begin_timestamp;
+        double End_timestamp;
+        off_t Logical_tail;
+        int ID_2;
+        off_t Physical_offset;
+};
+
+
+// Each index has its own signature
+class IdxSignature {
+    public:
+        IdxSignature():win_size(6) {} 
+        void discoverPattern( vector<off_t> const &seq );
+        SigStack<IdxSigUnit> discoverSigPattern( vector<off_t> const &seq,
+                vector<off_t> const &orig );
+        //It takes in a entry buffer like in PLFS,
+        //analyzes it and generate Index Signature Entries
+        IdxSigEntryList generateIdxSignature(vector<IdxEntry> &entry_buf, int proc);
+    private:
+        vector<IdxEntry> entry_buf;
+        int win_size; //window size
+        Tuple searchNeighbor( vector<off_t> const &seq,
+                vector<off_t>::const_iterator p_lookahead_win ); 
+};
+
+//This is the new entry for the new index
+//file using I/O signature. It corresponds to
+//HostEntry in old PLFS.
+//
+//Damn, where can I put the time stamp :(
+class IdxSigEntry {
+    public:
+        int memsize() 
+        {
+            return sizeof(proc) + logical_offset.memsize() 
+                + length.memsize() + physical_offset.memsize();
+        }
+
+    public:
+        int proc;
+        IdxSigUnit logical_offset;
+        SigStack<IdxSigUnit> length;
+        SigStack<IdxSigUnit> physical_offset;
+};
+
+
+class IdxSigEntryList {
+    public:
+        vector<IdxSigEntry> list;
+        idxfile::EntryList pb_list;
+
+    public:
+        void append(IdxSigEntryList other);
+        void append(vector<IdxSigEntry> &other);
+        void show();
+        //TODO:
+        void saveToFile(char *filename);
+        void readFromFile(char *filename);
+        void siglistToPblist(vector<IdxSigEntry> &slist,
+                idxfile::EntryList &pblist);
+};
+
+void printIdxEntries( vector<IdxSigEntry> &idx_entry_list );
+vector<off_t> buildDeltas( vector<off_t> seq );
+#endif
+
